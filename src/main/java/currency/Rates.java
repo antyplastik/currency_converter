7de37bdf698d708ebeac606c0b6d8b40d4fd1066
Ptaq <antyplastik@gmail.com>
2019-01-09 00:23:06 +0100

    timestamp

--- src/main/java/currency/Rates.java
+++ src/main/java/currency/Rates.java
@@ -3,12 +3,11 @@ package currency;
 public class Rates {
 
     private String no;              // – numer tabeli
-
     private String tradingDate;     // – data notowania (dotyczy tabeli C)
     private String effectiveDate;   // – data publikacji
     private String bid;             // – przeliczony kurs kupna waluty (dotyczy tabeli C)
-    private String ask;             // – przeliczony kurs sprzedaży waluty (dotyczy tabeli C)
-    private String mid;             // – przeliczony kurs średni waluty (dotyczy tabel A oraz B)       X
+    private String  ask;             // – przeliczony kurs sprzedaży waluty (dotyczy tabeli C)
+    private String  mid;             // – przeliczony kurs średni waluty (dotyczy tabel A oraz B)       X
 
     public Rates(String no, String tradingDate, String effectiveDate, String bid, String ask, String mid) {
         this.no = no;
